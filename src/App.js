68a2ac0723ab076761061a631d8b307245107dca
changwoonchoi <zzzmaster@snu.ac.kr>
2022-08-08 21:06:58 +0900

    fix typo in email address

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           </div>
           <div className="button-wrapper">
             <div className="buttons">
-              <a href="mailto: changwoon.choi00@gmjail.com" className="image-text-button" target="_blank">
+              <a href="mailto: changwoon.choi00@gmail.com" className="image-text-button" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" className="pictogram" width="24" height="24" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z"/></svg>
                 MAIL
               </a>
